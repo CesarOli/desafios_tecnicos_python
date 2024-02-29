b9db0420ea184905734b666a1a6d9b8820008d1e
CesarOli <cesaraleoliveira@gmail.com>
2024-02-28 23:27:22 -0300

    Inclusão do enunciado do desafio um.

---
+++ ringaII/desafio1.py
@@ -0,0 +1,16 @@
+'''Escreva uma função que recebe um array não vazio de inteiros distintos e um inteiro representando 
+uma soma alvo. Se quaisquer dois números no array de entrada somam a soma a soma alvo, a função
+deve retorná-los no array de saíd, em qualquer ordem. Se nenhum par de números no array de entrada
+somarem a soma alvo, a função deve retornar um array vazio.
+
+Note que a soma alvo deve ser obtida somando dois inteiros diferentes do array, cove pode soma 
+um inteiro com ele mesmo. Voce pode assumir que existirá no máximo um par de números somando
+a soma alvo.
+
+Exemplos de entrada
+array = [3, 5, -4, 8, 11, -1, 6]
+targetSum = 10
+
+Exemplo de saída
+[-1, 11] // pode ser ordem invertida
+'''
