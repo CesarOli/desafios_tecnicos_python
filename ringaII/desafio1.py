629122bc759023c511d812d5c2c041a671b2eec4
CesarOli <cesaraleoliveira@gmail.com>
2024-03-05 23:55:27 -0300

    Resolução do desafio 1.

--- ringaII/desafio1.py
+++ ringaII/desafio1.py
@@ -14,3 +14,28 @@ targetSum = 10
 Exemplo de saída
 [-1, 11] // pode ser ordem invertida
 '''
+
+import random
+
+def procura_por_par_soma_alvo(lista, soma_alvo):
+    rastreia_numeros = set()
+
+    for numero in lista:
+        diferenca_alvo = soma_alvo - numero
+
+        if diferenca_alvo in rastreia_numeros:
+            return [numero, diferenca_alvo]
+        else:
+            rastreia_numeros.add(numero)
+
+    return []
+
+# Exemplo de uso com array e soma alvo gerados aleatoriamente:
+tamanho_lista = 7  # Defina o tamanho desejado para o array
+lista_de_exemplo = [random.randint(-50, 50) for _ in range(tamanho_lista)]  # Gera números aleatórios entre -50 e 50
+soma_alvo_de_exemplo = random.randint(-100, 100)  # Gera uma soma alvo aleatória entre -100 e 100
+
+resultado = procura_por_par_soma_alvo(lista_de_exemplo, soma_alvo_de_exemplo)
+print(f"Array de exemplo: {lista_de_exemplo}")
+print(f"Soma alvo de exemplo: {soma_alvo_de_exemplo}")
+print(f"Resultado: {resultado}")
